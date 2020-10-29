4d06c4b949d8de0db8b0283fcc1c7305120e0368
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2020-10-28 23:09:10 -0300

    adicionado sistema de queue e

--- Lab 3/Example/LAB3/main.c
+++ Lab 3/Example/LAB3/main.c
@@ -7,6 +7,7 @@
 /* FreeRTOS.org includes. */
 #include "FreeRTOS.h"
 #include "task.h"
+#include <stdbool.h>
 
 /* Demo includes. */
 #include "supporting_functions.h"
@@ -15,26 +16,45 @@
 #define LONG_TIME 0xffff
 #define TICKS_TO_WAIT    10
 
-SemaphoreHandle_t xSemaphore = NULL;
-
-
 /* Used as a loop counter to create a very crude delay. */
 #define mainDELAY_LOOP_COUNT		( 0xffffff )
 
 /* The task functions. */
 void vTask1( void *pvParameters );
 void vTask2( void *pvParameters );
+void vATask( void *pvParameters);
 
-void vTaskFunction(void* pvParameters);
+void vLiftAnalyzer(void* pvParameters);
 
 const char* pcTextForTask1 = "Task 1 is running \r\n";
 const char* pcTextForTask2 = "Task 2 is running \r\n";
 
+typedef struct
+{
+	boolean PortaFechada;
+	char Nome[6];  
+	int	 Andar;
+} Elevador; 
+
+QueueHandle_t queueAnalyzer;//Objeto da queue
 /*-----------------------------------------------------------*/
 
 int main( void )
 {
+	Elevador* elevador1 = malloc(sizeof * elevador1);
+	//char nome_cliente[] = "Fulano";
+
+	elevador1->Nome[0] = 'f';
+	elevador1->Nome[1] = 'u';
+	elevador1->Nome[2] = 'l';
+	elevador1->Nome[3] = 'a';
+	elevador1->Nome[4] = 'n';
+	elevador1->Nome[5] = 'o';
+	elevador1->Andar = 5;
+	elevador1->PortaFechada = false;
+
 	/* Create one of the two tasks. */
+	queueAnalyzer = xQueueCreate(10, sizeof(uint32_t));//Cria a queue *buffer* com 10 slots de 4 Bytes
 
 	xTaskCreate(	vTask1,		/* Pointer to the function that implements the task. */
 					"Task 1",	/* Text name for the task.  This is to facilitate debugging only. */
@@ -46,10 +66,23 @@ int main( void )
 	xTaskCreate(	vTask2,		/* Pointer to the function that implements the task. */
 					"Task 2",	/* Text name for the task.  This is to facilitate debugging only. */
 					1000,		/* Stack depth - most small microcontrollers will use much less stack than this. */
-					NULL,		/* We are not using the task parameter. */
+					(void*)elevador1,		/* We are not using the task parameter. */
 					1,			/* This task will run at priority 1. */
 					NULL);		/* We are not using the task handle. */
 
+	xTaskCreate(	vATask,		/* Pointer to the function that implements the task. */
+					"Task A",	/* Text name for the task.  This is to facilitate debugging only. */
+					1000,		/* Stack depth - most small microcontrollers will use much less stack than this. */
+					(void*)elevador1,		/* We are not using the task parameter. */
+					1,			/* This task will run at priority 1. */
+					NULL);		/* We are not using the task handle. */
+
+	xTaskCreate(	vLiftAnalyzer,		/* Pointer to the function that implements the task. */
+					"Task analyzer",	/* Text name for the task.  This is to facilitate debugging only. */
+					1000,		/* Stack depth - most small microcontrollers will use much less stack than this. */
+					(void*)elevador1,		/* We are not using the task parameter. */
+					1,			/* This task will run at priority 1. */
+					NULL);		/* We are not using the task handle. */
 	/* Start the scheduler to start the tasks executing. */
 	vTaskStartScheduler();	
 
@@ -60,12 +93,13 @@ int main( void )
 	for( ;; );
 	return 0;
 }
+
 /*-----------------------------------------------------------*/
 
 void vTask1( void *pvParameters )
 {
-const char *pcTaskName = "Task 1 is running\r\n";
-volatile uint32_t ul;
+	const char *pcTaskName = "Task 1 is running\r\n";
+	volatile uint32_t ul;
 
 	for( ;; )
 	{
@@ -77,70 +111,51 @@ volatile uint32_t ul;
 
 void vTask2( void *pvParameters )
 {
-const char *pcTaskName = "Task 2 is running\r\n";
-volatile uint32_t ul;
+	const char *pcTaskName = "Task 2 is running\r\n";
+	volatile uint32_t ul;
 
+	Elevador* elevador = (Elevador *)pvParameters;
 	/* As per most tasks, this task is implemented in an infinite loop. */
 	for( ;; )
 	{
+		vPrintString(elevador->Nome);
 		/* Print out the name of this task. */
 		vPrintString( pcTaskName );
-
 		vTaskDelay(1000, portTICK_PERIOD_MS);
 	}
 }
 
-
-
-
-/* Repetitive task. */
 void vATask(void* pvParameters)
 {
-    /* We are using the semaphore for synchronisation so we create a binary
-    semaphore rather than a mutex.  We must make sure that the interrupt
-    does not attempt to use the semaphore before it is created! */
-    xSemaphore = xSemaphoreCreateBinary();
+	const char* pcTaskName = "Task A is running\r\n";
+	volatile uint32_t ul;
+	Elevador* elevador = (Elevador*)pvParameters;
 
-    for (;; )
-    {
-        /* We want this task to run every 10 ticks of a timer.  The semaphore
-        was created before this task was started.
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for (;; )
+	{
 
-        Block waiting for the semaphore to become available. */
-        if (xSemaphoreTake(xSemaphore, LONG_TIME) == pdTRUE)
-        {
-            /* It is time to execute. */
+		xQueueSend(queueAnalyzer, &elevador, portMAX_DELAY);
 
-                /* We have finished our task.  Return to the top of the loop where
-                we will block on the semaphore until it is time to execute
-                again.  Note when using the semaphore for synchronisation with an
-                ISR in this manner there is no need to 'give' the semaphore
-                back. */
-        }
-    }
+		vTaskDelay(1500, portTICK_PERIOD_MS);
+	}
 }
 
-/* Timer ISR */
-void vTimerISR(void* pvParameters)
+void vLiftAnalyzer(void* pvParameters)
 {
-    static char ucLocalTickCount = 0;
-    static BaseType_t xHigherPriorityTaskWoken;
+	Elevador* elevador;
+	while (1)
+	{
+		if (xQueueReceive(queueAnalyzer, &elevador, portMAX_DELAY) == pdPASS) {
 
-        /* Is it time for vATask() to run? */
-    xHigherPriorityTaskWoken = pdFALSE;
-    ucLocalTickCount++;
-    if (ucLocalTickCount >= TICKS_TO_WAIT)
-    {
-        /* Unblock the task by releasing the semaphore. */
-        xSemaphoreGiveFromISR(xSemaphore, &xHigherPriorityTaskWoken);
-
-        /* Reset the count so we release the semaphore again in 10 ticks
-        time. */
-        ucLocalTickCount = 0;
-    }
-
-    /* If xHigherPriorityTaskWoken was set to true you
-    we should yield.  The actual macro used here is
-    port specific. */
-    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+			vPrintString("\n--------------------------\n");
+			vPrintString("Nome: ");
+			vPrintString(elevador->Nome);
+			vPrintString("\nAndar: ");
+			char snum[5];
+			itoa(elevador->Andar, snum, 10);
+			vPrintString(snum);
+			vPrintString("\n--------------------------\n");
+		}
+	}
 }
